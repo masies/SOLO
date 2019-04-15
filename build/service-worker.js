f59836026733985f72ff8839eb67889d3c013528
SImone Masiero <masies@usi.ch>
2019-04-15 22:34:49 +0200

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/masies/SOLO/precache-manifest.a6015672038d2248381b6581ebded630.js"
+  "/SOLO/precache-manifest.3cd339654afb17c4cb2a755992eab6bc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/masies/SOLO/index.html", {
+workbox.routing.registerNavigationRoute("/SOLO/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
